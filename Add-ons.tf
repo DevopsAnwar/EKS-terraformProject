c7b99726ee034bd3fe47ab90ca7f75f2cf219c63
AnastasiyaOhal <59424429+AnastasiyaOhal@users.noreply.github.com>
2024-02-24 13:25:20 -0600

    Update Add-ons.tf

--- Add-ons.tf
+++ Add-ons.tf
@@ -1,14 +1,21 @@
-resource "aws_eks_addon" "vpc_cni" {
-  addon_name   = "vpc-cni"
-  cluster_name = "demo"
-  resolve_conflicts = "OVERWRITE"
-}
-
-resource "aws_eks_addon" "vpc_csi" {
-  addon_name   = "vpc-csi"
-  cluster_name = "demo"
-  resolve_conflicts = "OVERWRITE"
-}
+  cluster_addons = {
+    #coredns = {
+    #  addon_version = "v1.8.7-eksbuild.3"
+    #  resolve_conflicts = "OVERWRITE"
+    #}
+    #kube-proxy = {
+    #  addon_version = "v1.24.7-eksbuild.2"
+    #  resolve_conflicts = "OVERWRITE"
+    #}
+    vpc-cni = {
+      addon_version = "v1.12.0-eksbuild.1"
+      resolve_conflicts = "OVERWRITE"
+    }
+    aws-ebs-csi-driver = {
+      addon_version = "v1.13.0-eksbuild.2"
+      resolve_conflicts="PRESERVE"
+    }
+  }
 
 
 
