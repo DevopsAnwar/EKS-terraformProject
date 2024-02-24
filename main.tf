46bddebc75a7959a65f8ae4ca09b6364f249f42b
AnastasiyaOhal <59424429+AnastasiyaOhal@users.noreply.github.com>
2024-02-24 12:31:17 -0600

    Update main.tf

--- main.tf
+++ main.tf
@@ -116,7 +116,7 @@ resource "aws_eks_node_group" "private-nodes" {
  resource "aws_launch_template" "eks-with-disks" {
    name = "eks-with-disks"
 
-   key_name = "local-provisioner"
+   key_name = "local"
 
    block_device_mappings {
      device_name = "/dev/xvdb"
