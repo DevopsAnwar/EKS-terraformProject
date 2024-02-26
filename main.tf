a326e76e100de38abeabefbe4d4f09b90dd4be7d
AnastasiyaOhal <59424429+AnastasiyaOhal@users.noreply.github.com>
2024-02-26 10:58:02 -0600

    Update main.tf

--- main.tf
+++ main.tf
@@ -132,13 +132,13 @@ resource "aws_eks_node_group" "private-nodes" {
 
 
 # This will allow granting IAM permissions based on the service account used by the pod
-data "tls_certificate" "eks" {
-  url = aws_eks_cluster.demo.identity[0].oidc[0].issuer
-}
+#data "tls_certificate" "eks" {
+#  url = aws_eks_cluster.demo.identity[0].oidc[0].issuer
+#}
 
-resource "aws_iam_openid_connect_provider" "eks" {
-  client_id_list  = ["sts.amazonaws.com"]
-  thumbprint_list = [data.tls_certificate.eks.certificates[0].sha1_fingerprint]
-  url             = aws_eks_cluster.demo.identity[0].oidc[0].issuer
-}
+#resource "aws_iam_openid_connect_provider" "eks" {
+#  client_id_list  = ["sts.amazonaws.com"]
+#  thumbprint_list = [data.tls_certificate.eks.certificates[0].sha1_fingerprint]
+#  url             = aws_eks_cluster.demo.identity[0].oidc[0].issuer
+#}
 
