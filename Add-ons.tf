124665e0df0bb842b84d52c0b278079876a06038
AnastasiyaOhal <59424429+AnastasiyaOhal@users.noreply.github.com>
2024-02-26 11:10:53 -0600

    Update Add-ons.tf

--- Add-ons.tf
+++ Add-ons.tf
@@ -7,25 +7,3 @@ resource "aws_eks_addon" "vpc_cni" {
 data "aws_iam_policy" "ebs_csi_policy" {
   arn = "arn:aws:iam::aws:policy/service-role/AmazonEBSCSIDriverPolicy"
 }
-
-module "irsa-ebs-csi" {
-  source  = "terraform-aws-modules/iam/aws//modules/iam-assumable-role-with-oidc"
-  version = "4.7.0"
-
-  create_role                   = true
-  role_name                     = "AmazonEKSTFEBSCSIRole-demo"
-  provider_url                  = replace(tls_certificate.rks.url, "https://", "")
-  role_policy_arns              = [data.aws_iam_policy.ebs_csi_policy.arn]
-  oidc_fully_qualified_subjects = ["system:serviceaccount:kube-system:ebs-csi-controller-sa"]
-}
-
-resource "aws_eks_addon" "ebs-csi" {
-  cluster_name             = "demo"
-  addon_name               = "aws-ebs-csi-driver"
-  addon_version            = "v1.5.2-eksbuild.1"
-  service_account_role_arn = module.irsa-ebs-csi.iam_role_arn
-  tags = {
-    "eks_addon" = "ebs-csi"
-    "terraform" = "true"
-  }
-}
