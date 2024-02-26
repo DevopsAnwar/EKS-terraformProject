79e7895e1a74b8ebcb2d4addf3b90fc1820b7929
AnastasiyaOhal <59424429+AnastasiyaOhal@users.noreply.github.com>
2024-02-26 11:49:25 -0600

    Update Add-ons.tf

--- Add-ons.tf
+++ Add-ons.tf
@@ -1,9 +1,38 @@
-resource "aws_eks_addon" "vpc_cni" {
-  addon_name   = "vpc-cni"
-  cluster_name = "demo"
-  resolve_conflicts = "OVERWRITE"
-}
+#resource "aws_eks_addon" "vpc_cni" {
+#  addon_name   = "vpc-cni"
+#  cluster_name = "demo"
+#  resolve_conflicts = "OVERWRITE"
+#}
 
-data "aws_iam_policy" "ebs_csi_policy" {
-  arn = "arn:aws:iam::aws:policy/service-role/AmazonEBSCSIDriverPolicy"
+variable "addons" {
+  type = list(object({
+    name    = string
+    version = string
+  }))
+
+  default = [
+    {
+      name    = "kube-proxy"
+      version = "v1.21.2-eksbuild.2"
+    },
+    {
+      name    = "vpc-cni"
+      version = "v1.10.1-eksbuild.1"
+    },
+    {
+      name    = "coredns"
+      version = "v1.8.4-eksbuild.1"
+    },
+    {
+      name    = "aws-ebs-csi-driver"
+      version = "v1.4.0-eksbuild.preview"
+    }
+  ]
+}
+resource "aws_eks_addon" "addons" {
+  for_each          = { for addon in var.addons : addon.name => addon }
+  cluster_name      = aws_eks_cluster.demo.id
+  addon_name        = each.value.name
+  addon_version     = each.value.version
+  resolve_conflicts = "OVERWRITE"
 }
