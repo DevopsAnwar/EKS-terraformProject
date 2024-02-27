3bc2df8cf3bb4338fce2d36f08cb58ec70c48eca
AnastasiyaOhal <59424429+AnastasiyaOhal@users.noreply.github.com>
2024-02-27 10:57:09 -0600

    Update main.tf

--- main.tf
+++ main.tf
@@ -24,7 +24,7 @@ resource "aws_iam_role_policy_attachment" "demo-AmazonEKSClusterPolicy" {
 
 resource "aws_eks_cluster" "demo" {
   name     = "demo"
-  version  = "1.23"
+  version  = "1.21"
   role_arn         = aws_iam_role.demo.arn
 
   vpc_config {
