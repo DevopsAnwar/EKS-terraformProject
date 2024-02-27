3fe84966e369d4819686bfd0e0efa3141fead6e6
AnastasiyaOhal <59424429+AnastasiyaOhal@users.noreply.github.com>
2024-02-27 11:35:41 -0600

    Update Add-ons.tf

--- Add-ons.tf
+++ Add-ons.tf
@@ -11,5 +11,18 @@ resource "aws_eks_addon" "csi" {
   addon_name        = "aws-ebs-csi-driver"
   addon_version    = "v1.20.0-eksbuild.1"
   resolve_conflicts = "OVERWRITE"
-
+ 
+  configuration_values = jsonencode({
+    replicaCount = 4
+    resources = {
+      limits = {
+        cpu    = "100m"
+        memory = "150Mi"
+      }
+      requests = {
+        cpu    = "100m"
+        memory = "150Mi"
+      }
+    }
+  })
 }
