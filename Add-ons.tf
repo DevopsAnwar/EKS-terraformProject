885d4bbeec469ea1d2906c017fd898354b81682e
AnastasiyaOhal <59424429+AnastasiyaOhal@users.noreply.github.com>
2024-02-26 18:00:45 -0600

    Update Add-ons.tf

--- Add-ons.tf
+++ Add-ons.tf
@@ -29,6 +29,7 @@ variable "addons" {
     }
   ]
 }
+
 resource "aws_eks_addon" "addons" {
   for_each          = { for addon in var.addons : addon.name => addon }
   cluster_name      = aws_eks_cluster.demo.id
