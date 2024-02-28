445c06ab1c01b420eeb65e6f7ca8ef8b1779d5a3
AnastasiyaOhal <59424429+AnastasiyaOhal@users.noreply.github.com>
2024-02-27 19:05:29 -0600

    Update Add-ons.tf

--- Add-ons.tf
+++ Add-ons.tf
@@ -6,9 +6,9 @@ resource "aws_eks_addon" "cni" {
 
 }
 
-#resource "aws_eks_addon" "csi" {
-#  cluster_name      = "demo"
-#  addon_name        = "aws-ebs-csi-driver"
-#  addon_version     = "v1.20.0-eksbuild.1"
-#  resolve_conflicts = "OVERWRITE"
-# }
+resource "aws_eks_addon" "csi" {
+  cluster_name      = "demo"
+  addon_name        = "aws-ebs-csi-driver"
+  addon_version     = "v1.20.0-eksbuild.1"
+  resolve_conflicts = "OVERWRITE"
+}
