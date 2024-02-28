1181de6195c94c5d15a5c29605210ebcbc639310
AnastasiyaOhal <59424429+AnastasiyaOhal@users.noreply.github.com>
2024-02-28 09:48:21 -0600

    Update Add-ons.tf

--- Add-ons.tf
+++ Add-ons.tf
@@ -1,15 +1,24 @@
-resource "aws_eks_addon" "cni" {
-  cluster_name      = "demo"
-  addon_name        = "vpc-cni"
-  addon_version     = "v1.16.0-eksbuild.1"
-  resolve_conflicts = "OVERWRITE"
+#resource "aws_eks_addon" "cni" {
+#  cluster_name      = "demo"
+#  addon_name        = "vpc-cni"
+#  addon_version     = "v1.16.0-eksbuild.1"
+#  resolve_conflicts = "OVERWRITE"
+#}
 
-}
+#resource "aws_eks_addon" "csi" {
+#  cluster_name      = "demo"
+#  addon_name        = "aws-ebs-csi-driver"
+#  addon_version     = "v1.20.0-eksbuild.1"
+#  resolve_conflicts = "OVERWRITE"
+#}
 
-resource "aws_eks_addon" "csi" {
-  cluster_name      = "demo"
-  addon_name        = "aws-ebs-csi-driver"
-  addon_version     = "v1.20.0-eksbuild.1"
-  resolve_conflicts = "OVERWRITE"
-}
-
+#resource "aws_eks_addon" "ebs-csi" {
+#  cluster_name             = "demo"
+#  addon_name               = "aws-ebs-csi-driver"
+#  addon_version            = "v1.20.0-eksbuild.1"
+#  service_account_role_arn = module.irsa-ebs-csi.iam_role_arn
+#  tags = {
+#    "eks_addon" = "ebs-csi"
+#    "terraform" = "true"
+#  }
+#}
