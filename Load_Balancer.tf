5f37e09bf489a3a31f0d798a599a5ec1792d4813
AnastasiyaOhal <59424429+AnastasiyaOhal@users.noreply.github.com>
2024-02-28 20:09:03 -0600

    Update Load_Balancer.tf

--- Load_Balancer.tf
+++ Load_Balancer.tf
@@ -13,7 +13,7 @@ module "lb_role" {
 }
 
 provider "kubernetes" {
-  host                   = var.cluster_endpoint
+  host                   = module.eks.cluster_endpoint
   cluster_ca_certificate = base64decode(var.cluster_ca_cert)
   exec {
     api_version = "client.authentication.k8s.io/v1alpha1"
